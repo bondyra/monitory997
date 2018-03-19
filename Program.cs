8be2cf65509f397926b6ce73c738c6e17806e334
Jakub Bondyra <jb10193@gmail.com>
2018-03-19 13:16:29 +0100

    add monitory997 solution

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace monitory997
+{
+    public class Monitory
+    {
+        private void logEvent(String eventText)
+        {
+            System.Console.WriteLine(System.DateTime.Now + " " + eventText);
+        }
+
+        public void run ()
+        {   
+            logEvent("Hello Monitory997!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var monitory = new Monitory();
+            monitory.run();
+        }
+    }
+}
