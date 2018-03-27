9854efecde6e99136932791f84670c67259dd1f3
Jakub Bondyra <jb10193@gmail.com>
2018-03-27 18:44:37 +0200

    getRandomMiliseconds

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ namespace monitory997
 {
     public class Monitory
     {
+        private Random r = new Random(997);
+
+        private int getRandomMiliseconds(double min, double max){
+            double m = r.NextDouble();
+            return (int)(1000*(min+m*(max-min)));
+        }
+
         private void logEvent(String eventText)
         {
             System.Console.WriteLine(System.DateTime.Now + " " + eventText);
